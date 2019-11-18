c2bf2619ec992276b1e2d3c26c87558725b92480
Alexandre Jardin <info@ajardin.fr>
2019-11-18 22:11:15 +0100

    Upgrade the Origami formula

--- Formula/origami.rb
+++ Formula/origami.rb
@@ -1,8 +1,8 @@
 class Origami < Formula
   desc "A PHP toolbox to manage local Docker environments."
   homepage "https://github.com/ajardin/origami"
-  url "https://github.com/ajardin/origami/releases/download/v0.1.0/origami.phar"
-  sha256 "adc6a23f1eec38247ba6ee426167feb9802d216987305d806deaeaf0a272abc4"
+  url "https://github.com/ajardin/origami/releases/download/v0.2.0/origami.phar"
+  sha256 "415f503b03c65ec59fda697d562817c0c45f6184a42437257c52d6e2a8c58f93"
 
   def install
     mv "origami.phar", "origami"
